232862b8568c77fadf71d198d2d26392e88946f7
José Vitor <jose.vitorw018@gmail.com>
2026-06-18 22:02:22 -0300

    Criado o Repository

---
+++ src/main/java/com/vtr/saas/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.vtr.saas.repositories;
+
+import com.vtr.saas.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, String> {
+}
