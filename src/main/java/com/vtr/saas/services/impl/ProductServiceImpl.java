18e6796fb891f021a1ba9ec706b1cf76b3f14102
José Vitor <jose.vitorw018@gmail.com>
2026-07-04 12:59:14 -0300

    finalizado o productService

--- src/main/java/com/vtr/saas/services/impl/ProductServiceImpl.java
+++ src/main/java/com/vtr/saas/services/impl/ProductServiceImpl.java
@@ -1,15 +1,19 @@
 package com.vtr.saas.services.impl;
 
+import com.vtr.saas.common.PageResponse;
 import com.vtr.saas.entities.Category;
 import com.vtr.saas.entities.Product;
-import com.vtr.saas.mappers.CategoryMapper;
 import com.vtr.saas.mappers.ProductMapper;
 import com.vtr.saas.repositories.CategoryRepository;
 import com.vtr.saas.repositories.ProductRepository;
 import com.vtr.saas.requests.ProductRequest;
-import jakarta.validation.constraints.NotBlank;
+import com.vtr.saas.responses.ProductResponse;
+import com.vtr.saas.services.ProductService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,20 +23,64 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Slf4j
 @Transactional
-public class ProductServiceImpl {
+public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
     private final ProductMapper productMapper;
 
-    public void create (final ProductRequest productRequest){
-        checkIfProductExists(productRequest.getReference());
-        checkIfCategoryExists(productRequest.getCategoryId());
+    @Override
+    public void create(final ProductRequest productRequest){
+        checkIfProductAlreadyExistsByReference(productRequest.getReference());
+        checkIfCategoryExistById(productRequest.getCategoryId());
         final Product entity = this.productMapper.toEntity(productRequest);
         this.productRepository.save(entity);
     }
 
-    private void checkIfCategoryExists( final String categoryId ) {
+    @Override
+    public void update(final String id, final ProductRequest request) {
+        final Optional<Product> productExists = this.productRepository.findById(id);
+        if (productExists.isEmpty()) {
+            log.debug("Product does not exist");
+            throw new EntityNotFoundException("Product does not exist");
+        }
+
+        if (!productExists.get().getReference().equalsIgnoreCase(request.getReference())) {
+            checkIfProductAlreadyExistsByReference(request.getReference());
+        }
+
+        checkIfCategoryExistById(request.getCategoryId());
+
+        final Product productToUpdate = this.productMapper.toEntity(request);
+        productToUpdate.setId(id);
+        this.productRepository.save(productToUpdate);
+
+    }
+
+    @Override
+    public PageResponse<ProductResponse> findAll(int page, int size) {
+        final PageRequest pageRequest = PageRequest.of(page, size);
+        final Page<Product> products = this.productRepository.findAll(pageRequest);
+        final Page<ProductResponse> productsResponses = products.map(this.productMapper::toResponse);
+        return PageResponse.of(productsResponses);
+    }
+
+    @Override
+    public ProductResponse findById(String id) {
+        return this.productRepository.findById(id)
+                .map(this.productMapper::toResponse)
+                .orElseThrow(() -> new EntityNotFoundException("Product does not exist"));
+    }
+
+    @Override
+    public void delete(String id) {
+        final Product product = this.productRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Product does not exist"));
+        this.productRepository.delete(product);
+    }
+
+
+    private void checkIfCategoryExistById( final String categoryId ) {
         final Optional<Category> category = categoryRepository.findById(categoryId);
         if(category.isEmpty()) {
             log.debug("Category does not exist");
@@ -40,7 +88,7 @@ public class ProductServiceImpl {
         }
     }
 
-    private void checkIfProductExists(final String refence){
+    private void checkIfProductAlreadyExistsByReference(final String refence){
         final Optional<Product> product = productRepository.findByReferenceIgoneCase(refence);
             if (product.isPresent()){
                 log.debug("Product Already existing");
