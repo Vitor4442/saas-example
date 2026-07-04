017df52d5fcc0d4d45ab81a6b99d5ec1eacd15bb
José Vitor <jose.vitorw018@gmail.com>
2026-07-03 21:04:15 -0300

    Feito os repositories

---
+++ src/main/java/com/vtr/saas/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.vtr.saas.repositories;
+
+import com.vtr.saas.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, String> {
+    Optional<Product> findByReferenceIgoneCase(String reference);
+}
---
+++ src/main/java/com/vtr/saas/repositories/StockMvtRepository.java
@@ -0,0 +1,7 @@
+package com.vtr.saas.repositories;
+
+import com.vtr.saas.entities.StockMvt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockMvtRepository extends JpaRepository<StockMvt, Long> {
+}
