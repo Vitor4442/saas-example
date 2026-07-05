d09d10a6c0c39a1aa5c8dac88a781e483cb00aa3
José Vitor <jose.vitorw018@gmail.com>
2026-07-05 12:32:37 -0300

    Feito o categoryController

--- src/main/java/com/vtr/saas/controllers/CategoryController.java
+++ src/main/java/com/vtr/saas/controllers/CategoryController.java
@@ -1,20 +1,21 @@
 package com.vtr.saas.controllers;
 
+import com.vtr.saas.common.PageResponse;
 import com.vtr.saas.requests.CategoryRequest;
 import com.vtr.saas.responses.CategoryResponse;
 import com.vtr.saas.services.CategoryService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
 @RequiredArgsConstructor
-@Slf4j
+@Tag(name = "Category", description = "Category API")
 public class CategoryController {
 
     private final CategoryService service;
@@ -26,24 +27,24 @@ public class CategoryController {
     }
 
     @PutMapping("/{category-id}")
-    public ResponseEntity<Void> updatedCategory(@Valid @RequestBody final CategoryRequest request, @PathVariable("category-id") final String id){
+    public ResponseEntity<Void> updatedCategory(@Valid @RequestBody final CategoryRequest request, @PathVariable("category-id")  @NotNull(message = "Category ID cannot be null") final String id){
         this.service.update(id, request);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{category-id}")
-    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable("category-id") final String id){
+    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable("category-id") @NotNull(message = "Category ID cannot be null") final String id){
         return ResponseEntity.ok(this.service.findById(id));
     }
 
     @DeleteMapping("/{category-id}")
-    public  ResponseEntity<Void> deleteCategory( @PathVariable("category-id") final String id){
+    public  ResponseEntity<Void> deleteCategory( @PathVariable("category-id")  @NotNull(message = "Category ID cannot be null") final String id){
         this.service.delete(id);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping()
-    public ResponseEntity<List<CategoryResponse>> getAllCategory(){
-        return ResponseEntity.ok(this.service.findAll());
+    public ResponseEntity<PageResponse<CategoryResponse>> getAllCategory(@RequestParam(name = "page", defaultValue = "0") final int page, @RequestParam(name = "size", defaultValue = "10") final int size){
+        return ResponseEntity.ok(this.service.findAll(page, size));
     }
 }
