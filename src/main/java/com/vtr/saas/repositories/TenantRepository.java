d3542d794aaae14f0751a8d8ffe4d62259175638
José Vitor <jose.vitorw018@gmail.com>
2026-07-08 21:40:44 -0300

    Feito os repositorys de User e Tenant

---
+++ src/main/java/com/vtr/saas/repositories/TenantRepository.java
@@ -0,0 +1,12 @@
+package com.vtr.saas.repositories;
+
+import com.vtr.saas.entities.Tenant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TenantRepository extends JpaRepository<Tenant, String> {
+
+
+    boolean existsByCompanyCode(String companyCode);
+
+    boolean existsByEmail(String email);
+}
