f1ed5018145d1dbad40c66da504bad565d6ff242
José Vitor <jose.vitorw018@gmail.com>
2026-07-10 22:31:37 -0300

    Feito a logica dos erros

---
+++ src/main/java/com/vtr/saas/exceptions/DuplicateResourceException.java
@@ -0,0 +1,7 @@
+package com.vtr.saas.exceptions;
+
+public class DuplicateResourceException extends BusinessException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
