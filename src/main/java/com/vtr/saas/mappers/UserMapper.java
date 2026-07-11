1e46616baacfa77589c447e78878d0895b2be2a2
José Vitor <jose.vitorw018@gmail.com>
2026-07-10 23:26:25 -0300

    Feito o mapper

---
+++ src/main/java/com/vtr/saas/mappers/UserMapper.java
@@ -0,0 +1,34 @@
+package com.vtr.saas.mappers;
+
+import com.vtr.saas.entities.User;
+import com.vtr.saas.requests.UserRequest;
+import com.vtr.saas.responses.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserMapper {
+    public User toEntity(final UserRequest request) {
+        return User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .role(request.getRole())
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .build();
+    }
+
+    public UserResponse toResponse(final User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .role(user.getRole())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .build();
+    }
+}
